8931024a0ef6f34818797942523b2a77d72fc5c7
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-09-27 19:47:21 +0530

    update all changes

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Intro from "../Intro/Intro";
+
+function Home() {
+  return (
+    <div className="">
+      <Navbar />
+      <Intro />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div>
+      <main role="main" class="inner cover mt-5">
+        <h1 class="cover-heading">DAPPLED</h1>
+        <p class="lead">
+          the dynamic duo of design and development, 
+          who collaborate <br /> to turn creative visions into stunning digital
+          realities.
+        </p>
+        <p class="lead">
+          <a href="#" class="btn btn-lg btn-secondary">
+            Learn more
+          </a>
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default Intro;
