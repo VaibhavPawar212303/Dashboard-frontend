59c283917370ec4d6e42cbf491b1516bce5726ec
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-10-26 15:07:48 +0530

    updated few route data

--- src/components/Siderbar/Sidebar.jsx
+++ src/components/Siderbar/Sidebar.jsx
@@ -4,7 +4,6 @@ import logo from "../images/Testrig-logo-white.svg";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
 function Sidebar() {
-
   return (
     <div class="container-fluid w-full">
       <div class="row flex-nowrap">
@@ -15,22 +14,21 @@ function Sidebar() {
               class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
             >
               <span class="fs-5 d-none d-sm-inline">
-                <img src={logo} width={200} height={50} />
+                <img src={logo} width={150} height={50} />
               </span>
             </a>
             <ul
               class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
               id="menu"
             >
-              <li>
+              {/* <li>
                 <a href="/" class="nav-link px-0 align-middle">
                   <i class="fs-4 bi-speedometer2 text-white"></i>{" "}
                   <span class="ms-1 d-none d-sm-inline text-white">
                     Dashboard
                   </span>{" "}
                 </a>
-              </li>
-
+              </li> */}
               <li>
                 <a href="/project" class="nav-link px-0 align-middle">
                   <i class="fs-4 bi-grid text-white"></i>
