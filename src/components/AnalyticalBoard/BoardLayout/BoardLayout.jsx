87972d7d0b0520334678ca8f794bca22a5c27bbe
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-11-02 22:55:52 +0530

    updated header and other button details

--- src/components/AnalyticalBoard/BoardLayout/BoardLayout.jsx
+++ src/components/AnalyticalBoard/BoardLayout/BoardLayout.jsx
@@ -1,14 +1,37 @@
 import React from "react";
-import Linegraph from "../Linegraph/Linegraph";
 import Bars from "../Bars/Bars";
 import Piechart from "../Piechart/Piechart";
 import Buildcount from "../Buildcount/Buildcount";
 import Footer from "../../Footer/Footer";
 import Latestbuild from "../Latestbuild/Latestbuild";
+import { useNavigate } from "react-router-dom";
 
 function BoardLayout() {
+  const navigate = useNavigate();
   return (
     <div>
+      <div className="d-flex justify-content-end px-5">
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+          onClick={() => navigate(-1)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrow-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            ></path>
+          </svg>
+          Previous...
+        </button>
+      </div>
       <div>
         <Buildcount />
       </div>
--- src/components/AnalyticalBoard/Latestbuild/Allbuild.jsx
+++ src/components/AnalyticalBoard/Latestbuild/Allbuild.jsx
@@ -1,6 +1,9 @@
 import { React, useEffect, useState } from "react";
+import Footer from "../../Footer/Footer";
+import { useNavigate } from "react-router-dom";
 
 function Allbuild() {
+  const navigate = useNavigate();
   let projectID = localStorage.getItem("ProjectID");
   const setBuildId = (id) => {
     //set build id
@@ -27,6 +30,28 @@ function Allbuild() {
 
   return (
     <div className="mr-5">
+      <div className="d-flex justify-content-end px-5">
+        <button
+          type="button"
+          class="btn btn-outline-secondary"
+          onClick={() => navigate(-1)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-arrow-left"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+            ></path>
+          </svg>
+          Previous...
+        </button>
+      </div>
       <h4 className="mb-3 mt-3">Build Executed</h4>
       <table class="table">
         <thead class="thead-dark">
@@ -74,6 +99,7 @@ function Allbuild() {
           </tbody>
         ))}
       </table>
+      <Footer/>
     </div>
   );
 }
