cead49c32a2e4ecfa88900a4bade26b9cb171e9f
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-11-04 19:43:36 +0530

    updatd UI changes again

--- src/components/Siderbar/Sidebar.jsx
+++ src/components/Siderbar/Sidebar.jsx
@@ -7,7 +7,7 @@ function Sidebar() {
   return (
     <div class="container-fluid w-full">
       <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0"  style={{backgroundColor:"#42bd8b"}}>
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0"  style={{backgroundColor:"#5dda83"}}>
           <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             <a
               href="/"
